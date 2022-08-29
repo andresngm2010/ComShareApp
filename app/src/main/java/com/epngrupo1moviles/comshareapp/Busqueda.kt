1c2a9615a477b980df4b984888546c56aa703275
andresngm2010 <andres.gomez@epn.edu.ec>
2022-08-29 00:18:07 -0500

    Búsqueda de comunidades

--- app/src/main/java/com/epngrupo1moviles/comshareapp/Busqueda.kt
+++ app/src/main/java/com/epngrupo1moviles/comshareapp/Busqueda.kt
@@ -5,11 +5,19 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class Busqueda :  AppCompatActivity(){
+    lateinit var editTextBusqueda: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_busqueda)
@@ -18,6 +26,8 @@ class Busqueda :  AppCompatActivity(){
         val email = extras.getString("email") ?:"Unknown"
         val provider = extras.getString("provider") ?:"Unknown"
 
+        editTextBusqueda = findViewById<EditText>(R.id.editViewBuscar)
+
         val buttonHome = findViewById<ImageButton>(R.id.imageButtonHome)
         buttonHome.setOnClickListener {
             val prIntent : Intent = Intent(this,PantallaPrincipal::class.java).apply {
@@ -25,6 +35,12 @@ class Busqueda :  AppCompatActivity(){
                 putExtra("provider", provider)
             }
             startActivity(prIntent)
+        }
+
+        val buttonBuscar = findViewById<ImageButton>(R.id.imageButtonBusqueda)
+        buttonBuscar.setOnClickListener{
+            val busqueda:String = editTextBusqueda.text.toString()
+            cargarBusqueda(busqueda)
         }
 
         val buttonTodasComunidades = findViewById<ImageButton>(R.id.imageButtonTodasComunidades)
@@ -42,9 +58,63 @@ class Busqueda :  AppCompatActivity(){
         }
     }
 
-    fun cambiarAEjComunidad(v: View?){
-        val prIntent : Intent = Intent(this,EjemploComunidad::class.java)
-        startActivity(prIntent)
+    fun cargarBusqueda(name: String ){
+        limpiarBusqueda()
+        val db = Firebase.firestore
+        db.collection("Comunidades")
+            .get()
+            .addOnSuccessListener { documents ->
+                Toast.makeText(this, "Documentos con exito", Toast.LENGTH_LONG).show()
+                if(documents!= null && documents.documents.count()>0){
+                    var count = 1
+                    for (document in documents){
+                        if(count < 9){
+                            val nombre = document.getString("nombre").toString()
+                            val url = document.getString("URL").toString()
+                            if(nombre.contains(name)){
+                                val id = resources.getIdentifier("imageViewB$count", "id", packageName)
+                                val imagen = findViewById<ImageView>(id)
+                                Glide.with(applicationContext).load(url).into(imagen)
+                                CambiarEJComunidad(imagen, url, nombre)
+                                count++
+                            }
+                        }
+                    }
+                }
+
+            }
+
+    }
+
+    fun limpiarBusqueda(){
+        for( i in 1..8 ){
+            val id = resources.getIdentifier("imageViewB$i", "id", packageName)
+            val imagen = findViewById<ImageView>(id)
+            Glide.with(applicationContext).load("").into(imagen)
+            imagen.setOnClickListener {
+
+            }
+        }
+    }
+
+    //funcion para añadir onClickListener en los imageView
+    fun CambiarEJComunidad(v: ImageView, URL: String, nombreCom:String){
+        //obtenemos los extras enviados
+        val extras = intent.extras ?: return
+        val email = extras.getString("email") ?:"Unknown"
+        val provider = extras.getString("provider") ?:"Unknown"
+        //se añade el onClickListener
+        v.setOnClickListener {
+            //se cambia a la pantalla de la Comunidad
+            val prIntent : Intent = Intent(this,EjemploComunidad::class.java).apply {
+                //se añaden los extras necesarios
+                putExtra("email", email)
+                putExtra("provider", provider)
+                putExtra("url", URL)
+                putExtra("nombreCom", nombreCom)
+            }
+            startActivity(prIntent)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
